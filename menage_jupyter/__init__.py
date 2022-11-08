8d959c14dec15a67ae3d19cf9dbe8af5f6e9beb0
chwilko <73043312+chwilko@users.noreply.github.com>
2022-11-08 21:56:16 +0100

    Update __init__.py

--- menage_jupyter/__init__.py
+++ menage_jupyter/__init__.py
@@ -1,8 +1,9 @@
-from .clean import clean
+from .clean import clean, clean_all
 from .code import jupyter2py, py2jupyter
 
 __all__ = [
     py2jupyter,
     jupyter2py,
     clean,
+    clean_all,
 ]
