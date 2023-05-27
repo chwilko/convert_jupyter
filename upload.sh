6a73c496f335eedbd545812dffad3be4f5baac81
chwilko <bartekchwilkowski@gmail.com>
2023-05-27 22:46:36 +0200

    0.1.10

--- upload.sh
+++ upload.sh
@@ -10,5 +10,7 @@ python3 -m pip install --upgrade twine
 
 echo "use __token__ as username"
 echo "and token value as password"
-twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+python3 -m twine upload --repository-url https://upload.pypi.org/legacy/ dist/*
+
+# python -m twine upload dist/*
 
